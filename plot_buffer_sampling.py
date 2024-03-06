31e6b415f5a35c2f6a75f684ba146a9229e6bd0d
OrenNeumann <orennew@gmail.com>
2024-03-06 23:58:56 +0100

    Using Figure and config

--- plot_buffer_sampling.py
+++ plot_buffer_sampling.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from src.data_analysis.game_data_analysis import process_games_with_buffer
-from src.general.general_utils import models_path, fit_power_law
+from src.general.general_utils import models_path, game_path, fit_power_law
+from src.plotting.plot_utils import Figure
 
 """
 Plot state frequency with a buffer.
@@ -10,30 +11,27 @@ Plot state frequency with a buffer.
 # Choose game type:
 game_num = 0
 games = ['connect4', 'pentago', 'oware', 'checkers']
-
 env = games[game_num]
-# path = '/mnt/ceph/neumann/alphazero/scratch_backup/models/'
-# path = '/home/oren/zipf/scratch_backup/models/connect_four_10000/'
-path = models_path()
-data_paths = {'connect4': 'connect_four_10000/connect_four_10000/q_2_2',#'connect_four_10000/q_0_0',  
-              'pentago': 'pentago_t5_10000/q_0_0',
-              'oware': 'oware_10000/q_1_0',
-              'checkers': 'checkers/q_6_0'}
-path += data_paths[env]
-print('Collecting ' + env + ' games:')
 
+path = models_path() + game_path(env)
+model = 'q_6_2'
+path += model + '/'
+
+print('Collecting ' + env + ' games:')
 num_files = 39
+
 fit_colors = ['olivedrab', 'dodgerblue']
 data_colors = ['navy', 'darkviolet']
-plt.figure(figsize=(10, 6))
+fig = Figure(x_label='Board state number', 
+             y_label='Frequency',
+             title='Frequency of Board States, ' + env, 
+             legend=True)
+fig.preamble()
 
 board_counter, _ = process_games_with_buffer(env, path, sample_unique_states=False, max_file_num=num_files)
 
 # Sort by frequency
 board_freq = sorted(board_counter.items(), key=lambda x: x[1], reverse=True)
-
-# Extract the keys and the frequencies
-# keys = [item[0] for item in board_freq]
 freq = [item[1] for item in board_freq]
 
 # Fit a power-law
@@ -55,20 +53,13 @@ x = np.arange(n_points) + 1
 plt.scatter(x, freq[:n_points], color=data_colors[0], s=40 / (10 + x))
 plt.plot(x_fit, y_fit, color=fit_colors[0], linewidth=1.5, label='Uniform sampling, '+equation)
 
-
-
 ####### run again to get unique-sampling data: ###############
-
 
 board_counter, _ = process_games_with_buffer(env, path, sample_unique_states=True, max_file_num=num_files)
 
 # Sort by frequency
 board_freq = sorted(board_counter.items(), key=lambda x: x[1], reverse=True)
-
-# Extract the keys and the frequencies
-# keys = [item[0] for item in board_freq]
 freq = [item[1] for item in board_freq]
-
 
 # set fitting limits to capture the tail power-law:
 # omit last two plateaus (freq=1 or 2)
@@ -85,13 +76,10 @@ n_points = len(freq)  # 5 * 10 ** 6  # Display top n_points
 x = np.arange(n_points) + 1
 plt.scatter(x, freq[:n_points], color=data_colors[1], s=40 / (10 + x))
 plt.plot(x_fit, y_fit, color=fit_colors[1], linewidth=1.5, label='Unique sampling, '+equation)
-plt.ylabel('Frequency')
-plt.xlabel('Board state number')
+
 plt.xscale('log')
 plt.yscale('log')
-# plt.xlim(right=n_points)
-plt.title('Frequency of Board States, ' + env)
-plt.legend()
-plt.tight_layout()
-plt.savefig('plots/buffer_distribution.png', dpi=900)
+
+fig.epilogue()
+fig.save('buffer_distribution')
 plt.show()
--- src/plotting/plot_utils.py
+++ src/plotting/plot_utils.py
@@ -4,49 +4,9 @@ import scienceplots
 import numpy as np
 
 
-def figure_preamble(fig_num=1):
-    w, h = plt.figaspect(0.6)
-    plt.figure(fig_num, figsize=(w, h))
-    plt.style.use(['grid'])
-    plt.clf()
-
-
-def figure_epilogue(x_label='', y_label='', title='', label_font=18, number_font=16,legend=False):
-    if x_label is not '':
-        plt.xlabel(x_label, fontsize=label_font)
-    if y_label is not '':
-        plt.ylabel(y_label, fontsize=label_font)
-    if title is not '':
-        plt.title(title, fontsize=label_font)
-    if legend:
-        plt.legend()
-    plt.xticks(fontsize=number_font)
-    plt.yticks(fontsize=number_font)
-    plt.tight_layout()
-
-
-def bar_figure_preamble(par, label_font=18, number_font=16, fig_num=1):
-    # colorbar plot cargo-cult code
-    figure_preamble(fig_num)
-    # ax = plt.gca()
-    norm = matplotlib.colors.LogNorm(vmin=par.min(), vmax=par.max())
-    # create a scalarmappable from the colormap
-    sm = matplotlib.cm.ScalarMappable(cmap=plt.get_cmap('viridis'), norm=norm)
-    cbar = plt.colorbar(sm)
-    cbar.ax.tick_params(labelsize=number_font)
-    cbar.ax.set_ylabel('Parameters', rotation=90, fontsize=label_font)
-
-
-def colorbar_colors(par):
-    """ calculate colorbar colors."""
-    log_par = np.log(par)
-    color_nums = (log_par - log_par.min()) / (log_par.max() - log_par.min())
-    return color_nums
-
-
 class Figure:
     """
-    A class representing a figure for plotting.
+    Class for plotting a figure.
 
     Args:
         fig_num (int): The figure number.
@@ -58,8 +18,9 @@ class Figure:
         legend (bool): Whether to show the legend.
 
     Methods:
-        figure_preamble(): Set up the figure.
-        figure_epilogue(): Finalize the figure.
+        preamble(): Set up the figure.
+        epilogue(): Finalize the figure.
+        save(name): Save the figure.
     """
 
     def __init__(self,
@@ -85,11 +46,11 @@ class Figure:
         plt.clf()
 
     def epilogue(self):
-        if self.x_label is not '':
+        if self.x_label != '':
             plt.xlabel(self.x_label, fontsize=self.text_font)
-        if self.y_label is not '':
+        if self.y_label != '':
             plt.ylabel(self.y_label, fontsize=self.text_font)
-        if self.title is not '':
+        if self.title != '':
             plt.title(self.title, fontsize=self.text_font)
         if self.legend:
             plt.legend(fontsize=self.text_font - 3)
@@ -102,10 +63,12 @@ class Figure:
 
 class BarFigure(Figure):
     """
-    A class representing a bar figure.
+    Class for bar figures (figures with a colorbar on the side).
 
-    Args:
+    Extra Args:
         par (list or array): The parameter values for the bar.
+    Extra Methods:
+        colorbar_colors(): Calculate the color scale for the colorbar.
     """
 
     def __init__(self, par, fig_num=1, x_label='', y_label='', title='', text_font=18, number_font=16, legend=False):
