f1124cbd94c239f2c92c22062c93de4d85c08ffc
OrenNeumann <orennew@gmail.com>
2024-03-07 23:14:58 +0100

    Corrected frequency updates

--- src/data_analysis/state_frequency/buffer_counter.py
+++ src/data_analysis/state_frequency/buffer_counter.py
@@ -4,6 +4,12 @@ from src.data_analysis.state_frequency.state_counter import StateCounter
 
 
 class BufferCounter(StateCounter):
+    """ A StateCounter that samples states from a replay buffer.
+        
+        New Args:
+            sample_unique_states: bool, whether to sample unique states from the buffer rather
+            than sample normally.
+    """
     def __init__(self,
                  env: str,
                  save_serial=False,
@@ -22,24 +28,21 @@ class BufferCounter(StateCounter):
         self.buffer_reuse = 10
         self.sample_threshold = int(self.buffer_size / self.buffer_reuse)
         self.buffer = deque(maxlen=self.buffer_size)
-        self.new_states = 0
+        self.states_added = 0
 
     def reset_counters(self):
         super().reset_counters()
         self.buffer = deque(maxlen=self.buffer_size)
-        self.new_states = 0
+        self.states_added = 0
 
-    def _update_frequencies(self, board, key):
-        self.buffer.append(key)
-        self.new_states += 1
-
-        if self.new_states == self.sample_threshold:
-            self.new_states = 0
-            samples = self._sample_buffer()
-            for k in samples:
-                self.frequencies[k] += 1
-                if self.frequencies[k] == 1:
-                    self.serials[k] = board.serialize()
+    def _update_frequencies(self, keys):
+        """ Store keys in the buffer, performing updates when hitting the sample threshold."""
+        for key in keys:
+            self.buffer.append(key)
+            self.states_added += 1
+            if self.states_added == self.sample_threshold:
+                self.states_added = 0
+                super()._update_frequencies(self._sample_buffer())
 
     def _sample_buffer(self):
         if self.sample_unique_states:
@@ -48,10 +51,12 @@ class BufferCounter(StateCounter):
             return self._sample_uniformly()
 
     def _sample_unique_states(self):
+        """ Sample uniformly from the set of unique keys."""
         unique_keys = list(set(self.buffer))
         if len(unique_keys) < self.batch_size:
             raise Exception(f'Batch size ({self.batch_size}) is larger than number of unique keys ({len(unique_keys)}).')
         return sample(unique_keys, self.batch_size)
 
     def _sample_uniformly(self):
+        """ Conventional sampling."""
         return sample(self.buffer, self.batch_size)
