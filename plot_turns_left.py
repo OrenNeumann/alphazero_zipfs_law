3e1134650210cc977ead79bcdfdff643f9b20805
OrenNeumann <orennew@gmail.com>
2024-03-08 12:38:46 +0100

    Added pruning

--- plot_turns_left.py
+++ plot_turns_left.py
@@ -16,7 +16,8 @@ env = games[game_num]
 
 data_labels = [0, 1, 2, 3, 4, 5]  # for oware no 6
 
-state_counter = gather_data(env, data_labels, max_file_num=1, save_turn_num=True)
+state_counter = gather_data(env, data_labels, max_file_num=39, save_turn_num=True)
+state_counter.prune_low_frequencies(4)
 turns_played = state_counter.turns_played
 turns_to_end = state_counter.turns_to_end
 board_counter = state_counter.frequencies
