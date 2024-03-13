f0d7712b425262d38645c5b0931fe887ddc7c247
Oren Neumann <orennew@gmail.com>
2024-03-13 18:02:23 +0100

    For printing dkl data

--- plot_fast.py
+++ plot_fast.py
@@ -1,28 +1,83 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+from src.data_analysis.data_utils import sort_by_frequency
+from src.plotting.plot_utils import Figure
 
-with open(r"/home/oren/zipf/plot_data/erase/played_owar.pkl", "rb") as input_file:
+data_dir = r"/mnt/ceph/neumann/zipf/plot_data/erase/che_test/"
+with open(data_dir + "played.pkl", "rb") as input_file:
     played = pickle.load(input_file)
-with open(r"/home/oren/zipf/plot_data/erase/sample_owar.pkl", "rb") as input_file:
+with open(data_dir + "sampled.pkl", "rb") as input_file:
     sampled = pickle.load(input_file)
+with open(data_dir + "turns_played.pkl", "rb") as input_file:
+    turns_played = pickle.load(input_file)
+with open(data_dir + "turns_sampled.pkl", "rb") as input_file:
+    turns_sampled = pickle.load(input_file)
 
-plt.figure(2, figsize=(10, 6))
+print(len(played))
+print(len(sampled))
+print(len(turns_played))
+print(len(turns_sampled))
+
+print('done loading')
+for key, count in played.items():
+    turns_played[key] /= count
+for key, count in sampled.items():
+    turns_sampled[key] /= count
+
+font = 18 - 2
+font_num = 16 - 2
+
+fig = Figure(x_label='State rank', text_font=font, number_font=font_num)
+
+
+def new_fig(y_label):
+    fig.fig_num += 1
+    fig.preamble()
+
+    fig.y_label = y_label
+
+
+print('plotting zipfs law')
+new_fig('Frequency')
 freq = np.array([item[1] for item in played.most_common()])
-x = np.arange(len(played)) + 1
+#freq = freq[:10**4]
+x = np.arange(len(freq)) + 1
 plt.scatter(x, np.array(freq), s=40 / (10 + x), label=played)
-freq = np.array([item[1] for item in sampled.most_common()])
-x = np.arange(len(sampled)) + 1
-plt.scatter(x, np.array(sampled), s=40 / (10 + x), label=sampled)
-#played = list( c for  c in played if c >= 4)
-#sampled = list( c for  c in sampled if c >= 4)
 
-plt.ylabel('Frequency')
-plt.xlabel('Board state number')
+freq = np.array([item[1] for item in sampled.most_common()])
+#freq = freq[:10**4]
+x = np.arange(len(freq)) + 1
+plt.scatter(x, np.array(freq), s=40 / (10 + x), label=sampled)
+#plt.legend(loc="upper right")
+
+if fig.x_label != '':
+    plt.xlabel(fig.x_label, fontsize=fig.text_font)
+if fig.y_label != '':
+    plt.ylabel(fig.y_label, fontsize=fig.text_font)
+if fig.title != '':
+    plt.title(fig.title, fontsize=fig.text_font)
+if fig.legend:
+    plt.legend(fontsize=fig.text_font - 3)
+plt.xticks(fontsize=fig.number_font)
+plt.yticks(fontsize=fig.number_font)
 plt.xscale('log')
 plt.yscale('log')
-plt.title('Frequency of Board States')
-#plt.legend()
-#plt.tight_layout()
-plt.savefig('plots/counts.png', dpi=900)
+
+#fig.epilogue()
+fig.save('zipfs_law')
+
+print('plotting turns')
+new_fig('Turn')
+y = sort_by_frequency(data=turns_played, counter=sampled)
+x = np.arange(len(y)) + 1
+plt.scatter(x, y, s=40 * 3 / (10 + x), alpha=1, color='green')
+fig.epilogue()
+fig.save('turns_played')
+
+new_fig('Turn')
+y = sort_by_frequency(data=turns_sampled, counter=sampled)
+x = np.arange(len(y)) + 1
+plt.scatter(x, y, s=40 * 3 / (10 + x), alpha=1, color='green')
+fig.epilogue()
+fig.save('turns_sampled')
