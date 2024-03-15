d07405d47bea204e6f519c30a3f77330efda6940
OrenNeumann <orennew@gmail.com>
2024-03-15 19:08:10 +0100

    Compatible with base

--- src/alphazero_scaling/alphazero_mods/policy_sampling.py
+++ src/alphazero_scaling/alphazero_mods/policy_sampling.py
@@ -11,12 +11,12 @@ import sys
 import tempfile
 import time
 import traceback
-
-from src.alphazero_scaling.alphazero_base import AlphaZero, watcher, Buffer
+from open_spiel.python.algorithms.alpha_zero import model as model_lib
 from open_spiel.python.utils import data_logger
 from open_spiel.python.utils import spawn
 from open_spiel.python.utils import stats
 
+from src.alphazero_scaling.alphazero_base import AlphaZero, watcher, Buffer, JOIN_WAIT_DELAY
 from src.alphazero_scaling.sampling.kl_sampling import Sampler
 #from sampling.kl_sampling import Sampler
 from collections import Counter, defaultdict, namedtuple
@@ -45,37 +45,51 @@ class AlphaZeroKLSampling(AlphaZero):
         self.turns_played = defaultdict(int)
         self.turns_sampled = defaultdict(int)
 
-    def collect_trajectories(self, game_lengths, game_lengths_hist, outcomes, replay_buffer,
-                             value_accuracies, value_predictions):
+    def collect_trajectories(self, model):
         """Collects the trajectories from actors into the replay buffer."""
         num_trajectories = 0
         num_states = 0
         for trajectory in self.trajectory_generator():
             num_trajectories += 1
             num_states += len(trajectory.states)
-            game_lengths.add(len(trajectory.states))
-            game_lengths_hist.add(len(trajectory.states))
+            self.game_lengths.add(len(trajectory.states))
+            self.game_lengths_hist.add(len(trajectory.states))
 
             p1_outcome = trajectory.returns[0]
             if p1_outcome > 0:
-                outcomes.add(0)
+                self.outcomes.add(0)
             elif p1_outcome < 0:
-                outcomes.add(1)
+                self.outcomes.add(1)
             else:
-                outcomes.add(2)
+                self.outcomes.add(2)
 
-            replay_buffer.extend(
+            ###
+            sampled_states = self.sampler.kl_sampling(trajectory, model)
+            if self.count_states:
+                self.played_counter.update(str(s.observation) for s in trajectory.states)
+                self.sample_counter.update(str(s.observation) for s in sampled_states)
+                for s in trajectory.states:
+                    self.turns_played[str(s.observation)] += s.move_number
+                for s in sampled_states:
+                    self.turns_sampled[str(s.observation)] += s.move_number
+
+            # Warm-up, ignore the sampled states on the first step:
+            if len(self.replay_buffer) < self.learn_rate:
+                sampled_states = trajectory.states
+
+            self.replay_buffer.extend(
                 model_lib.TrainInput(
                     s.observation, s.legals_mask, s.policy, p1_outcome)
-                for s in trajectory.states)
+                for s in sampled_states)
+            ###
 
             for stage in range(self.stage_count):
                 # Scale for the length of the game
                 index = (len(trajectory.states) - 1) * stage // (self.stage_count - 1)
                 n = trajectory.states[index]
                 accurate = (n.value >= 0) == (trajectory.returns[n.current_player] >= 0)
-                value_accuracies[stage].add(1 if accurate else 0)
-                value_predictions[stage].add(abs(n.value))
+                self.value_accuracies[stage].add(1 if accurate else 0)
+                self.value_predictions[stage].add(abs(n.value))
 
             if num_states >= self.learn_rate:
                 break
@@ -85,7 +99,7 @@ class AlphaZeroKLSampling(AlphaZero):
     def learner(self, *, evaluators, broadcast_fn, logger):
         """A learner that consumes the replay buffer and trains the network."""
         logger.also_to_stdout = True
-        replay_buffer = Buffer(self.config.replay_buffer_size)
+        # Model must be initialized here, making it a class variable makes the run freeze
         logger.print("Initializing model")
         model = self._init_model_from_config(self.config)
         logger.print("Model type: %s(%s, %s)" % (self.config.nn_model, self.config.nn_width,
@@ -97,32 +111,28 @@ class AlphaZeroKLSampling(AlphaZero):
 
         data_log = data_logger.DataLoggerJsonLines(self.config.path, "learner", True)
 
-        value_accuracies = [stats.BasicStats() for _ in range(self.stage_count)]
-        value_predictions = [stats.BasicStats() for _ in range(self.stage_count)]
-        game_lengths = stats.BasicStats()
-        game_lengths_hist = stats.HistogramNumbered(self.game.max_game_length() + 1)
-        outcomes = stats.HistogramNamed(["Player1", "Player2", "Draw"])
         evals = [Buffer(self.config.evaluation_window) for _ in range(self.config.eval_levels)]
         total_trajectories = 0
 
         last_time = time.time() - 60
         for step in itertools.count(1):
-            for value_accuracy in value_accuracies:
+            for value_accuracy in self.value_accuracies:
                 value_accuracy.reset()
-            for value_prediction in value_predictions:
+            for value_prediction in self.value_predictions:
                 value_prediction.reset()
-            game_lengths.reset()
-            game_lengths_hist.reset()
-            outcomes.reset()
+            self.game_lengths.reset()
+            self.game_lengths_hist.reset()
+            self.outcomes.reset()
 
-            num_trajectories, num_states = self.collect_trajectories(game_lengths, game_lengths_hist, outcomes,
-                                                                     replay_buffer, value_accuracies, value_predictions)
+            num_trajectories, num_states = self.collect_trajectories(model)
             total_trajectories += num_trajectories
             now = time.time()
             seconds = now - last_time
             last_time = now
 
-            ratio, a, target = self.sampler.update_hyperparameters() ###
+            ###
+            ratio, a, target = self.sampler.update_hyperparameters()
+            ###
 
             logger.print("Step:", step)
             logger.print(
@@ -132,7 +142,7 @@ class AlphaZeroKLSampling(AlphaZero):
                                                   num_states / (self.config.actors * seconds),
                                                   num_states / num_trajectories))
             logger.print("Buffer size: {}. States seen: {}".format(
-                len(replay_buffer), replay_buffer.total_seen))
+                len(self.replay_buffer), self.replay_buffer.total_seen))
             ###
             logger.print("Sampling ratio: {:.5f}. Coeff.: {:.5f}, Target coeff.: {:.5f}.".format(
                 ratio, a, target))
@@ -141,7 +151,7 @@ class AlphaZeroKLSampling(AlphaZero):
                     len(self.played_counter), len(self.sample_counter)))
             ###
 
-            save_path, losses = self.learn(step, logger, replay_buffer, model)
+            save_path, losses = self.learn(step, logger, model)
 
             for eval_process in evaluators:
                 while True:
@@ -151,49 +161,49 @@ class AlphaZeroKLSampling(AlphaZero):
                     except spawn.Empty:
                         break
 
-            batch_size_stats = stats.BasicStats()  # Only makes sense in C++.
-            batch_size_stats.add(1)
-            data_log.write({
-                "step": step,
-                "total_states": replay_buffer.total_seen,
-                "states_per_s": num_states / seconds,
-                "states_per_s_actor": num_states / (self.config.actors * seconds),
-                "total_trajectories": total_trajectories,
-                "trajectories_per_s": num_trajectories / seconds,
-                "queue_size": 0,  # Only available in C++.
-                "game_length": game_lengths.as_dict,
-                "game_length_hist": game_lengths_hist.data,
-                "outcomes": outcomes.data,
-                "value_accuracy": [v.as_dict for v in value_accuracies],
-                "value_prediction": [v.as_dict for v in value_predictions],
-                "eval": {
-                    "count": evals[0].total_seen,
-                    "results": [sum(e.data) / len(e) if e else 0 for e in evals],
-                },
-                "batch_size": batch_size_stats.as_dict,
-                "batch_size_hist": [0, 1],
-                "loss": {
-                    "policy": losses.policy,
-                    "value": losses.value,
-                    "l2reg": losses.l2,
-                    "sum": losses.total,
-                },
-                "cache": {  # Null stats because it's hard to report between processes.
-                    "size": 0,
-                    "max_size": 0,
-                    "usage": 0,
-                    "requests": 0,
-                    "requests_per_s": 0,
-                    "hits": 0,
-                    "misses": 0,
-                    "misses_per_s": 0,
-                    "hit_rate": 0,
-                },
-            })
-            logger.print()
+            self._dump_statistics(logger, data_log, step, num_states, seconds, total_trajectories,
+                                  num_trajectories, evals, losses)
 
             if 0 < self.config.max_steps <= step:
                 break
 
             broadcast_fn(save_path)
 
+    def alpha_zero(self):
+        """Start all the worker processes for a full alphazero setup."""
+
+        actors = [spawn.Process(self.actor, kwargs={"num": i})
+                  for i in range(self.config.actors)]
+        evaluators = [spawn.Process(self.evaluator, kwargs={"num": i})
+                      for i in range(self.config.evaluators)]
+
+        def broadcast(msg):
+            for proc in actors + evaluators:
+                proc.queue.put(msg)
+
+        try:
+            self.actors = actors
+            self.learner(evaluators=evaluators, broadcast_fn=broadcast)
+        except (KeyboardInterrupt, EOFError):
+            print("Caught a KeyboardInterrupt, stopping early.")
+        finally:
+            with open(self.config.path + '/played.pkl', 'wb') as f:
+                pickle.dump(self.played_counter, f)
+            with open(self.config.path + '/sampled.pkl', 'wb') as f:
+                pickle.dump(self.sample_counter, f)
+            if self.count_states:
+                with open(self.config.path + '/turns_played.pkl', 'wb') as f:
+                    pickle.dump(self.turns_played, f)
+                with open(self.config.path + '/turns_sampled.pkl', 'wb') as f:
+                    pickle.dump(self.turns_sampled, f)
+            
+            broadcast("")
+        # for actor processes to join we have to make sure that their q_in is empty,
+        # including backed up items
+        for proc in actors:
+            while proc.exitcode is None:
+                while not proc.queue.empty():
+                    proc.queue.get_nowait()
+                proc.join(JOIN_WAIT_DELAY)
+        for proc in evaluators:
+            proc.join()
