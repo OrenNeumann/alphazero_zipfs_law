3dfee8f76df3c1206faa7955dd030614c4ee026e
OrenNeumann <orennew@gmail.com>
2024-03-16 20:07:59 +0100

    Less memory

--- plot_fast.py
+++ plot_fast.py
@@ -7,16 +7,11 @@ from src.plotting.plot_utils import Figure
 """
 Plot data from dkl sampling experiment
 """
-print('loading data')
-data_dir = r"/mnt/ceph/neumann/zipf/plot_data/sampling_counters/oware/s_3_2/"
-with open(data_dir + "played.pkl", "rb") as input_file:
-    played = pickle.load(input_file)
-with open(data_dir + "sampled.pkl", "rb") as input_file:
-    sampled = pickle.load(input_file)
 
-print('done loading')
-print(len(played))
-print(len(sampled))
+data_dir = r"/mnt/ceph/neumann/zipf/plot_data/sampling_counters/oware/s_3_2/"
+def load_data(name):
+    with open(data_dir + name+ ".pkl", "rb") as input_file:
+        return pickle.load(input_file)
 
 
 font = 18 - 2
@@ -27,18 +22,27 @@ fig = Figure(x_label='State rank',
              text_font=font,
              number_font=font_num,
              legend=True)
-
-print('plotting zipfs law')
 fig.preamble()
-freq = np.array([item[1] for item in played.most_common()])
+print('plotting zipfs law')
 
-x = np.arange(len(freq)) + 1
-plt.scatter(x, np.array(freq), s=40 / (10 + x), label='played')
+print('loading played')
+counts = load_data("played")
+print('length: ', len(counts))
 
-freq = np.array([item[1] for item in sampled.most_common()])
+print('plotting played')
 
-x = np.arange(len(freq)) + 1
-plt.scatter(x, np.array(freq), s=40 / (10 + x), label='sampled')
+
+x = np.arange(len(counts)) + 1
+plt.scatter(x, np.array([item[1] for item in counts.most_common()]), 
+            s=40 / (10 + x), label='played')
+
+print('loading sampled')
+counts = load_data("sampled")
+print('plotting sampled')
+
+x = np.arange(len(counts)) + 1
+plt.scatter(x, np.array([item[1] for item in counts.most_common()]), 
+            s=40 / (10 + x), label='sampled')
 
 plt.xscale('log')
 plt.yscale('log')
@@ -47,12 +51,11 @@ fig.save('zipfs_law')
 
 print('plotting turns')
 
-with open(data_dir + "turns_played.pkl", "rb") as input_file:
-    turns = pickle.load(input_file)
 
+turns = load_data("turns_sampled")
 
 print(len(turns))
-for key, count in played.items():
+for key, count in counts.items():
     turns[key] /= count
 
 
@@ -63,29 +66,29 @@ fig = Figure(fig_num=2,
              number_font=font_num)
 
 fig.preamble()
-y = sort_by_frequency(data=turns, counter=played)
-x = np.arange(len(y)-1) + 1
-plt.scatter(x, y[1:], s=40 * 3 / (10 + x), alpha=1, color='green')
-plt.xscale('log')
-plt.yscale('log')
-fig.epilogue()
-fig.save('turns_played')
-
-
-with open(data_dir + "turns_sampled.pkl", "rb") as input_file:
-    turns = pickle.load(input_file)
-print(len(turns))
-for key, count in sampled.items():
-    turns[key] /= count
-
-
-fig.fig_num = 3
-fig.preamble()
-y = sort_by_frequency(data=turns, counter=sampled)
-
+y = sort_by_frequency(data=turns, counter=counts)
 x = np.arange(len(y)-1) + 1
 plt.scatter(x, y[1:], s=40 * 3 / (10 + x), alpha=1, color='green')
 plt.xscale('log')
 plt.yscale('log')
 fig.epilogue()
 fig.save('turns_sampled')
+
+turns = load_data("turns_played")
+counts = load_data("played")
+print(len(turns))
+for key, count in counts.items():
+    turns[key] /= count
+
+
+fig.fig_num = 3
+fig.preamble()
+y = sort_by_frequency(data=turns, counter=counts)
+
+x = np.arange(len(y)-1) + 1
+plt.scatter(x, y[1:], s=40 * 3 / (10 + x), alpha=1, color='green')
+plt.xscale('log')
+plt.yscale('log')
+fig.epilogue()
+
+fig.save('turns_played')
