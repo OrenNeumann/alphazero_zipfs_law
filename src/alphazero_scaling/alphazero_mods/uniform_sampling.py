fb515f68f70a3cb41cf1b2f0c1c1c42ca7b67771
OrenNeumann <orennew@gmail.com>
2024-03-24 21:27:02 +0100

    Sampling batches without repetition

--- src/alphazero_scaling/alphazero_mods/uniform_sampling.py
+++ src/alphazero_scaling/alphazero_mods/uniform_sampling.py
@@ -6,11 +6,20 @@ from collections import defaultdict
 
 """
 Train a model with uniform state sampling, ignoring repetitions in the replay buffer.
+There are 2 improvements here on the base algorithm:
+
+1. The replay buffer is sampled without repeating states. Copies of the same state are averaged together.
+
+2. Each state in the buffer is sampled only once during optimization. In the base algorithm, data is sampled 
+several (~64) times from the buffer every optimization step, so that the total number of samples is the size 
+of the buffer. This adds a lot of redundancy in the trained data because of repeating samples.
+Here, the buffer is shuffled once and then split into batches, so all states in each batch are unique.
 """
 
 class UniqueBuffer(base.Buffer):
     def __init__(self, max_size):
         super().__init__(max_size)
+        self.unique_states = list()
 
     def sample(self, count):
         """ Sample without repetitions, using the most recent copy of each state but averaging the value."""
@@ -40,4 +49,22 @@ class AlphaZeroUniformSampling(base.AlphaZero):
         super()._print_step(logger, *args, **kwargs)
         logger.print("Duplicates: {}. Repeating states: {}.".format(
             n_duplicates, n_repeating))
+
+    def learn(self, step, logger, model):
+        """Sample batches without repetitions"""
+        losses = []
+        batch_size = self.config.train_batch_size
+        # Sample the entire (shuffled) buffer:
+        buffer_data = self.replay_buffer.sample(len(self.replay_buffer))
+        # Feed batches to the model:
+        for i in range(len(buffer_data) // batch_size):
+            data = buffer_data[i * batch_size: (i + 1) * batch_size]
+            losses.append(model.update(data))
+
+        save_path = model.save_checkpoint(
+            step if step % self.config.checkpoint_freq == 0 else -1)
+        losses = sum(losses, model_lib.Losses(0, 0, 0)) / len(losses)
+        logger.print(losses)
+        logger.print("Checkpoint saved:", save_path)
+        return save_path, losses
 
