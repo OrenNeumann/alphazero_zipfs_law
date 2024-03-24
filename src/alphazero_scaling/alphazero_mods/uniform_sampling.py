9951a841bf05cac68c14d6c8c7dcc34c80c68c43
OrenNeumann <orennew@gmail.com>
2024-03-24 14:36:39 +0100

    Added stats printout

--- src/alphazero_scaling/alphazero_mods/uniform_sampling.py
+++ src/alphazero_scaling/alphazero_mods/uniform_sampling.py
@@ -1,6 +1,6 @@
 import random
 import src.alphazero_scaling.alphazero_base as base
-
+from collections import Counter
 
 class UniqueBuffer(base.Buffer):
     def __init__(self, max_size):
@@ -13,7 +13,22 @@ class UniqueBuffer(base.Buffer):
             unique_dict[state.observation] = state
         return random.sample(list(unique_dict.values()), count)
     
+    def count_duplicates(self):
+        """ Count the number of duplicate states in the buffer."""
+        observations = [state.observation for state in self.data]
+        counts = Counter(observations)
+        duplicates = sum(val - 1 for val in counts.values() if val > 1)
+        repeat_observations = sum(1 for val in counts.values() if val > 1)
+        return duplicates, repeat_observations
+    
 class AlphaZeroUniformSampling(base.AlphaZero):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.replay_buffer = UniqueBuffer(self.config.replay_buffer_size)
+
+    def _print_step(self, logger, *args, **kwargs):
+        n_duplicates, n_repeating = self.replay_buffer.count_duplicates()
+        super()._print_step(logger, *args, **kwargs)
+        logger.print("Duplicates: {}. Repeating states: {}.".format(
+            n_duplicates, n_repeating))
+
