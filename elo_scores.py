653c691b81a02a242fa5e237acd52a42c8e274de
Oren Neumann <orennew@gmail.com>
2024-03-30 13:36:33 +0100

    Score extraction finished

--- elo_scores.py
+++ elo_scores.py
@@ -9,11 +9,13 @@ checkpoints = [20, 30, 50, 70, 100, 150, 230, 340, 510, 770, 1150, 1730, 2590, 3
 dir_name = '../matches/oware_base/'
 r = bayeselo.ResultSet()
 
-def add_match(n,m,p):
+
+def add_match(n, m, p):
     for k in range(800):
         # just say we had 1600 games and none of them tied:
-        r.append(n, m, int(k < p*800) * 2)
-        r.append(m, n, int(k > p*800) * 2)
+        r.append(n, m, int(k < p * 800) * 2)
+        r.append(m, n, int(k > p * 800) * 2)
+
 
 class PlayerNums(object):
     def __init__(self):
@@ -21,14 +23,14 @@ class PlayerNums(object):
         self.d = dict()
 
     def add(self, model, checkpoint):
-        player = model+'/'+str(checkpoint)
+        player = model + '/' + str(checkpoint)
         if player not in self.d:
             self.d[player] = self.n
             self.n += 1
-    
+
     def num(self, model, checkpoint):
-        return self.d[model+'/'+str(checkpoint)]
-    
+        return self.d[model + '/' + str(checkpoint)]
+
     def names(self):
         """ return dict keys in order of values """
         return sorted(self.d, key=self.d.get)
@@ -42,8 +44,6 @@ for i in range(6):
     for j in range(4):
         fixed_size_models.append('q_' + str(i) + '_' + str(j))
 
-
-
 # this assumes 1) that the matrix is symmetric 2) len(matches) = len(checkpoints)
 for model in tqdm(fixed_size_models, desc='Loading fixed-size matches'):
     matches = np.load(dir_name + 'fixed_size/' + str(model) + '/matrix.npy')
@@ -51,11 +51,10 @@ for model in tqdm(fixed_size_models, desc='Loading fixed-size matches'):
         agents.add(model, cp)
     if len(matches) != len(checkpoints):
         raise ValueError('Matrix size does not match number of checkpoints.')
-    for i,j in combinations(range(len(matches)), 2):
-        p = matches[i, j]
+    for i, j in combinations(range(len(matches)), 2):
         num_i = agents.num(model, checkpoints[i])
         num_j = agents.num(model, checkpoints[j])
-        add_match(num_i, num_j, p)
+        add_match(num_i, num_j, p=matches[i, j])
 
 
 def fc_model_ordering():
@@ -73,6 +72,7 @@ def fc_model_ordering():
                 nets.append('f_' + str(i) + '_' + str(j))
     return nets
 
+
 fixed_checkpoint_models = fc_model_ordering()
 
 # assumes len(matches) = len(fixed_checkpoint_models)
@@ -82,12 +82,10 @@ for cp in tqdm(checkpoints, desc='Loading fixed-checkpoint matches'):
         agents.add(model, cp)
     if len(matches) != len(fixed_checkpoint_models):
         raise ValueError('Matrix size does not match number of models.')
-    for i,j in combinations(range(len(matches)), 2):
-        p = matches[i, j]
+    for i, j in combinations(range(len(matches)), 2):
         num_i = agents.num(fixed_checkpoint_models[i], cp)
         num_j = agents.num(fixed_checkpoint_models[j], cp)
-        add_match(num_i, num_j, p)
-
+        add_match(num_i, num_j, p=matches[i, j])
 
 e = bayeselo.EloRating(r, agents.names())
 e.offset(1000)
@@ -95,17 +93,16 @@ e.mm()
 e.exact_dist()
 print(e)
 
+
 def _extract_elo(elo_rating):
-    x = elo_rating.__str__().split("\n")
-    table = []
-    for row in x:
-        table.append(row.split())
-    table = np.array(table[:-1])
-    agent_order = table[:, 1]
-    elo = table[:, 2]
-    agent_order = agent_order[1:].astype(int)
-    elo = elo[1:].astype(float)
-    elo = elo[agent_order.argsort()]
-    return elo
+    x = str(elo_rating).split("\n")
+    players = [row.split()[1] for row in x[1:-1]]
+    scores = [int(row.split()[2]) for row in x[1:-1]]
+    elos = dict(zip(players, scores))
+    return elos
+
 
 elo = _extract_elo(e)
+
+
+
