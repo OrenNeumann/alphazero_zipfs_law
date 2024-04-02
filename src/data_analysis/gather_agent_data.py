e1cc142c77d283aedcf2ff98a66fde6140b2eab0
Oren Neumann <orennew@gmail.com>
2024-04-02 18:22:03 +0200

    Added cutoff counter

--- src/data_analysis/gather_agent_data.py
+++ src/data_analysis/gather_agent_data.py
@@ -1,16 +1,23 @@
 from src.data_analysis.state_frequency.state_counter import StateCounter
+from src.data_analysis.state_frequency.counting_mods.cutoff_counter import CutoffCounter
 from src.general.general_utils import models_path, game_path
 
 
-def gather_data(env: str, labels: list[int], max_file_num: int = 1, save_serial: bool = False,
+def gather_data(env: str, labels: list[int], mod=None, max_file_num: int = 1, save_serial: bool = False,
                 save_turn_num: bool = False, save_value: bool = False) -> StateCounter:
     """ Gather data from multiple agents, aggregated together.
         Counts and averages are taken over all agents in 'labels'.
         Currently just uses agent copy num. 0."""
     print('Collecting ' + env + ' games:')
-    path = models_path() + game_path(env) + '/oware/ur_'#'q_'
-    state_counter = StateCounter(env=env, save_serial=save_serial, save_turn_num=save_turn_num, save_value=save_value,
-                                 cut_early_games=False)
+    path = models_path() + game_path(env) + 'q_'  # '/oware/ur_'
+    if mod is None:
+        state_counter = StateCounter(env=env, save_serial=save_serial, save_turn_num=save_turn_num,
+                                     save_value=save_value, cut_early_games=False)
+    elif mod == 'cutoff':
+        state_counter = CutoffCounter(env=env, save_serial=save_serial, save_turn_num=save_turn_num,
+                                      save_value=save_value, cut_early_games=False)
+    else:
+        raise ValueError("mod must be None or 'cutoff'")
     for label in labels:
         num = str(label)
         print("label " + num)
@@ -18,6 +25,4 @@ def gather_data(env: str, labels: list[int], max_file_num: int = 1, save_serial:
         state_counter.collect_data(path=agent_path, max_file_num=max_file_num)
 
     state_counter.normalize_counters()
-
     return state_counter
-
---
+++ src/data_analysis/state_frequency/counting_mods/cutoff_counter.py
@@ -0,0 +1,28 @@
+import re
+from src.data_analysis.state_frequency.state_counter import StateCounter
+
+
+class CutoffCounter(StateCounter):
+    """
+    Count states when using late-state cutoff.
+    'Cutoff' is the number of turns before the end of the game to stop counting states.
+    """
+    def __init__(self, cutoff=30, **kwargs):
+        super().__init__(**kwargs)
+        self.cutoff = cutoff
+
+    def _process_game(self, game_record):
+        """ Process a single game, counting states only until the cutoff point.
+        """
+        board = self.game.new_initial_state()
+        actions = re.findall(self.action_string, game_record)
+        keys = list()
+        for action in actions[:-self.cutoff]:
+            board.apply_action(board.string_to_action(action))
+            key = str(board)
+            keys.append(key)
+            if self.save_serial and key not in self.serials.keys():
+                self.serials[key] = board.serialize()
+        for action in actions[-self.cutoff:]:
+            board.apply_action(board.string_to_action(action))
+        return board, keys
