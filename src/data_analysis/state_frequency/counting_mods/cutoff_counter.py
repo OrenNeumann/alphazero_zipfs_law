7d8788445f15f908800e0e3263bf79a2c413bb6e
OrenNeumann <orennew@gmail.com>
2024-04-03 00:31:47 +0200

    Added final-steps cutoff

--- src/data_analysis/state_frequency/counting_mods/cutoff_counter.py
+++ src/data_analysis/state_frequency/counting_mods/cutoff_counter.py
@@ -6,10 +6,12 @@ class CutoffCounter(StateCounter):
     """
     Count states when using late-state cutoff.
     'Cutoff' is the number of turns before the end of the game to stop counting states.
+    end_tolerance cuts-off short games by restricting the distance to the final turn.
     """
     def __init__(self, cutoff=80, **kwargs):
         super().__init__(**kwargs)
         self.cutoff = cutoff
+        self.end_tolerance = 1
 
     def _process_game(self, game_record):
         """ Process a single game, counting states only until the cutoff point.
@@ -17,12 +19,16 @@ class CutoffCounter(StateCounter):
         board = self.game.new_initial_state()
         actions = re.findall(self.action_string, game_record)
         keys = list()
-        for action in actions[:self.cutoff]:
+        if len(actions) < self.cutoff:
+            cut = len(actions) - self.end_tolerance
+        else:
+            cut = self.cutoff
+        for action in actions[:cut]:
             board.apply_action(board.string_to_action(action))
             key = str(board)
             keys.append(key)
             if self.save_serial and key not in self.serials.keys():
                 self.serials[key] = board.serialize()
-        for action in actions[self.cutoff:]:
+        for action in actions[cut:]:
             board.apply_action(board.string_to_action(action))
         return board, keys
