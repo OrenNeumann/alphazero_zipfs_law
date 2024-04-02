ef706b8cf808663143981967c8d23686b9e831b1
OrenNeumann <orennew@gmail.com>
2024-04-03 01:59:18 +0200

    Fixed min/max bug

--- src/alphazero_scaling/alphazero_mods/turn_cutoff.py
+++ src/alphazero_scaling/alphazero_mods/turn_cutoff.py
@@ -13,12 +13,12 @@ class AlphaZeroTurnCutoff(base.AlphaZero):
     terminal state is not included in training).
     Cutoff is disabled every so often, so the agent can learn to play late-game states.
     """
-    def __init__(self, cutoff=3, disable_rate=0.01, end_tolerance=0, **kwargs):
+    def __init__(self, cutoff=50, disable_rate=0.01, end_tolerance=0, **kwargs):
         super().__init__(**kwargs)
         self.cutoff = cutoff
         self.end_tolerance = end_tolerance
         self.disable_rate = disable_rate
-    """
+
     def trajectory_generator(self):
         while True:
             found = 0
@@ -26,7 +26,7 @@ class AlphaZeroTurnCutoff(base.AlphaZero):
                 try: ###
                     trajectory = actor_process.queue.get_nowait()
                     if random.random() > self.disable_rate:
-                        cut = max(self.cutoff, len(trajectory.states) - self.end_tolerance)
+                        cut = min(self.cutoff, len(trajectory.states) - self.end_tolerance)
                         trajectory.states = trajectory.states[:cut]
                     yield trajectory
                     ###
@@ -36,56 +36,3 @@ class AlphaZeroTurnCutoff(base.AlphaZero):
                     found += 1
             if found == 0:
                 time.sleep(0.01)  # 10ms
-    """
-
-
-    def collect_trajectories(self, model):
-        """Collects the trajectories from actors into the replay buffer.
-            'model' may be used in derived classes."""
-        num_trajectories = 0
-        num_states = 0
-        for trajectory in self.trajectory_generator():
-            ###
-            check = True
-            l = len(trajectory.states)
-            
-            if random.random() > self.disable_rate:
-                cut = max(self.cutoff, len(trajectory.states) - self.end_tolerance)
-                trajectory.states = trajectory.states[:cut]
-            else:
-                check = False
-            if check:
-                if l> cut:
-                    if len(trajectory.states) != cut:
-                        raise ValueError(f"Cut-off failed: {len(trajectory.states)} -> {l}") 
-                    ###
-            num_trajectories += 1
-            num_states += len(trajectory.states)
-            self.game_lengths.add(len(trajectory.states))
-            self.game_lengths_hist.add(len(trajectory.states))
-
-            p1_outcome = trajectory.returns[0]
-            if p1_outcome > 0:
-                self.outcomes.add(0)
-            elif p1_outcome < 0:
-                self.outcomes.add(1)
-            else:
-                self.outcomes.add(2)
-
-            self.replay_buffer.extend(
-                model_lib.TrainInput(
-                    s.observation, s.legals_mask, s.policy, p1_outcome)
-                for s in trajectory.states)
-
-            for stage in range(self.stage_count):
-                # Scale for the length of the game
-                index = (len(trajectory.states) - 1) * stage // (self.stage_count - 1)
-                n = trajectory.states[index]
-                accurate = (n.value >= 0) == (trajectory.returns[n.current_player] >= 0)
-                self.value_accuracies[stage].add(1 if accurate else 0)
-                self.value_predictions[stage].add(abs(n.value))
-
-            if num_states >= self.learn_rate:
-                break
-        return num_trajectories, num_states
-
