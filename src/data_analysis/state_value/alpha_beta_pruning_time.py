b4e6ed63cc0ee89404cdea5a947a6c91989e3de4
OrenNeumann <orennew@gmail.com>
2024-04-05 23:54:25 +0200

    Timing limit added

--- src/data_analysis/state_value/alpha_beta_pruning_time.py
+++ src/data_analysis/state_value/alpha_beta_pruning_time.py
@@ -1,13 +1,11 @@
-import stat
 import numpy as np
 import subprocess
 from subprocess import PIPE
 import time
-import random
 import pickle
 from src.data_analysis.state_frequency.state_counter import StateCounter
 from src.plotting.plot_utils import Figure, incremental_bin
-from src.general.general_utils import models_path, game_path
+from src.general.general_utils import models_path
 import collections
 
 import matplotlib.pyplot as plt
@@ -20,8 +18,12 @@ Time how long it takes for alpha beta pruning to analyze connect4 states.
 game = pyspiel.load_game('connect_four')
 
 def time_alpha_beta_pruning(states):
+    """ Time the solver on states until 2 minutes have passed."""
     times = []
+    total_time = time.time()
     for state in states:
+        if time.time() - total_time > 2*60:
+            break
         moves = ""
         for action in state.full_history():
             # The solver starts counting moves from 1:
@@ -30,8 +32,9 @@ def time_alpha_beta_pruning(states):
         _ = subprocess.run(["src/alphazero_scaling/connect4-master/call_solver.sh", moves], stdout=PIPE, stderr=PIPE)
         time_end = time.time()
         times.append(time_end - time_start)
+
     print(times)
-    return np.mean(times)
+    return np.mean(times), np.std(times)
     
 
 def save_pruning_time():
@@ -39,16 +42,15 @@ def save_pruning_time():
     board_counter = collections.Counter()
     serial_states = dict()
     state_counter = StateCounter(env, save_serial=True)
-    for label in [0]:#, 2, 4, 6]:
+    for label in [0, 2, 4, 6]:
         num = str(label)
         path = models_path() + '/connect_four_10000/q_' + num + '_0/'
-        state_counter.collect_data(path=path, max_file_num=1)
+        state_counter.collect_data(path=path, max_file_num=2)
         # add counts to the counter, and update new serial states:
         board_counter.update(state_counter.frequencies)
         serial_states.update(state_counter.serials)
 
-    n = len(board_counter)
-    x = np.arange(n) + 1
+    state_counter.prune_low_frequencies(10)
 
     font = 18 - 2
     font_num = 16 - 2
@@ -60,34 +62,42 @@ def save_pruning_time():
                 number_font=font_num,
                 legend=True,
                 fig_num=2)
-    """
+    
     freq = np.array([item[1] for item in board_counter.most_common()])
+    x = np.arange(len(board_counter)) + 1
     plt.scatter(x, freq, s=40 / (10 + x))
     plt.xscale('log')
     plt.yscale('log')
     fig.epilogue()
-    fig.save('zipf_distribution')"""
+    fig.save('zipf_distribution')
 
     bins = incremental_bin(10 ** 10)
     widths = (bins[1:] - bins[:-1])
     bin_x = bins[:-1] + widths / 2
     keys = np.array([key for key,_ in board_counter.most_common()])
     times = []
-    indices = np.arange(30,50)
+    standard_devs = []
+    indices = np.arange(20,50)
     #for i in tqdm(indices,desc='Calculating times'):
     for i in indices:
         print('loop',i)
         state_keys = keys[int(bin_x[i]):int(bin_x[i+1])]
-        n = min(10,len(state_keys)) # for 2 random indices
+        n = min(100,len(state_keys))
         print('n=',n)
-        index = np.random.choice(state_keys.shape[0], n, replace=False)  
-        state_keys = state_keys[index]
-        states = [game.deserialize_state(serial_states[key]) for key in state_keys]
-        times.append(time_alpha_beta_pruning(states))
-
+        sample = np.random.choice(state_keys.shape[0], n, replace=False)  
+        states = [game.deserialize_state(serial_states[key]) for key in state_keys[sample]]
+        t, stdv = time_alpha_beta_pruning(states)
+        times.append(t)
+        standard_devs.append(stdv)
+    with open('../plot_data/ab_pruning.pkl', 'wb') as f:
+        pickle.dump({'x': bin_x[indices],
+                     'times': times,
+                     'std': standard_devs,
+                     'counter': state_counter}, f)
     fig.fig_num += 1
     fig.preamble()
-    plt.plot(bin_x[indices], times)
+    #plt.plot(bin_x[indices], times)
+    plt.errorbar(bin_x[indices], times, yerr=[standard_devs, standard_devs], fmt='-o')
     plt.xscale('log')
     plt.yscale('log')
     fig.y_label = 'CPU time (s)'
