64af39764d4defcf9b818ef396838c3ac67a9ace
OrenNeumann <orennew@gmail.com>
2024-04-06 02:31:17 +0200

    Fixed state counter bug

--- src/data_analysis/state_value/alpha_beta_pruning_time.py
+++ src/data_analysis/state_value/alpha_beta_pruning_time.py
@@ -9,7 +9,7 @@ from src.general.general_utils import models_path
 import collections
 import scipy
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+#from tqdm import tqdm
 import pyspiel
 """
 Time how long it takes for alpha beta pruning to analyze connect4 states.
@@ -20,9 +20,9 @@ game = pyspiel.load_game('connect_four')
 def time_alpha_beta_pruning(states, max_time=2*60):
     """ Time the solver on states until max_time seconds have passed."""
     times = []
-    total_time = time.time()
+    beginning = time.time()
     for state in states:
-        if time.time() - total_time > max_time:
+        if time.time() - beginning > max_time:
             break
         moves = ""
         for action in state.full_history():
@@ -32,25 +32,23 @@ def time_alpha_beta_pruning(states, max_time=2*60):
         _ = subprocess.run(["src/alphazero_scaling/connect4-master/call_solver.sh", moves], stdout=PIPE, stderr=PIPE)
         time_end = time.time()
         times.append(time_end - time_start)
-
     print(times)
     if len(times) == 1:
         gstd = 1
     else:
         gstd = scipy.stats.gstd(times)
-
     return np.mean(times), np.std(times), gstd
     
 
 def save_pruning_time():
     # Gather states
-    generate = False
+    generate = True
     if generate:
         generate_states()
-    with open('../plot_data/ab_pruning/counters.pkl', 'rb') as f:
-        counters = pickle.load(f)
-    board_counter = counters['counter']
-    serial_states = counters['serials']
+    with open('../plot_data/ab_pruning/counter.pkl', 'rb') as f:
+        state_counter = pickle.load(f)
+    board_counter = state_counter.frequencies
+    serial_states = state_counter.serials
 
     # Calculate solver times
     bins = incremental_bin(10 ** 10)
@@ -72,9 +70,11 @@ def save_pruning_time():
         n = min(100,len(state_keys))
         print('n=',n)
         sample = rng.choice(state_keys, n, replace=False) 
+        if len(sample) < 10:
+            sample = np.concatenate([sample,sample])
         states = [game.deserialize_state(serial_states[key]) for key in sample]
         if i < 15:
-            t, stdv, gstd= time_alpha_beta_pruning(states, max_time=4*60)
+            t, stdv, gstd= time_alpha_beta_pruning(states, max_time=3*60)
         else:
             t, stdv, gstd= time_alpha_beta_pruning(states)
         times.append(t)
@@ -104,23 +104,16 @@ def save_pruning_time():
     
 
 def generate_states():
-    env = 'connect_four'
-    board_counter = collections.Counter()
-    serial_states = dict()
-    state_counter = StateCounter(env, save_serial=True)
-    for label in [0, 2, 4, 6]:
+    state_counter = StateCounter(env='connect_four', save_serial=True)
+    for label in [0, 1, 2, 3, 4, 5, 6]:
         num = str(label)
         path = models_path() + '/connect_four_10000/q_' + num + '_0/'
-        state_counter.collect_data(path=path, max_file_num=10)
-        board_counter.update(state_counter.frequencies)
-        serial_states.update(state_counter.serials)
+        state_counter.collect_data(path=path, max_file_num=5)
 
     # Prune
-    board_counter = collections.Counter({k: c for k, c in board_counter.items() if c >= 10})
-    serial_states = {k: v for k, v in serial_states.items() if k in board_counter.keys()}
-    with open('../plot_data/ab_pruning/counters.pkl', 'wb') as f:
-        pickle.dump({'counter': board_counter,
-                        'serials': serial_states}, f)
+    state_counter.prune_low_frequencies(10)
+    with open('../plot_data/ab_pruning/counter.pkl', 'wb') as f:
+        pickle.dump(state_counter, f)
     font = 16
     font_num = 14
     print('Plotting zipf distribution')
@@ -130,8 +123,8 @@ def generate_states():
                 number_font=font_num,
                 legend=True,
                 fig_num=2)
-    freq = np.array([item[1] for item in board_counter.most_common()])
-    x = np.arange(len(board_counter)) + 1
+    freq = np.array([item[1] for item in state_counter.frequencies.most_common()])
+    x = np.arange(len(state_counter.frequencies)) + 1
     plt.scatter(x, freq, s=40 / (10 + x))
     plt.xscale('log')
     plt.yscale('log')
