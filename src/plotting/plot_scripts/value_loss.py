51d933ed3967157824828e8183077e0ca0549396
OrenNeumann <orennew@gmail.com>
2024-04-13 19:21:56 +0200

    Added all plots to connect4 plot

--- src/plotting/plot_scripts/value_loss.py
+++ src/plotting/plot_scripts/value_loss.py
@@ -1,10 +1,10 @@
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import pickle
 import numpy as np
-from tqdm import tqdm
 import scienceplots
-from src.plotting.plot_utils import aligned_title, smooth
+from src.plotting.plot_utils import aligned_title, smooth, gaussian_average
 
 plt.style.use(['science','nature','grid'])
 
@@ -16,40 +16,74 @@ def connect4_loss_plots():
 
     with open('../plot_data/ab_pruning/data.pkl', 'rb') as f:
         ab_data = pickle.load(f)
+    par = np.load('src/config/parameter_counts/connect_four.npy')
+    log_par = np.log(par)
+    color_nums = (log_par - log_par.min()) / (log_par.max() - log_par.min())
 
     titles = [r'$\bf{a.}$ Value loss on train set',
               r'$\bf{b.}$ Value loss on ground truth',
               r'$\bf{c.}$ Alpha-beta-pruning complexity']
     for i, ax in enumerate(axs):
-        x = ab_data['x']
-        y = np.array(ab_data['g_mean'])#
-        gstd = np.array(ab_data['gstd'])
-        err = np.array([y*(1-1/gstd), y*(gstd-1)])
-        c = -15 # Cut off hardware-limit plateau
-        ax.errorbar(x[:c], y[:c], yerr=err[:,:c], fmt='-o')
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        ax.tick_params(axis='both', which='major', labelsize=tf-2)
-        if i==0:
+        if i == 0:
+            print('Plotting training loss')
+            with open('../plot_data/value_loss/training_loss/loss_curves_connect_four.pkl', 'rb') as f:
+                loss_curves = pickle.load(f)
+            for label in [0, 1, 2, 3, 4, 5, 6]:
+                curves = [np.array(loss_curves[f'q_{label}_{copy}']) for copy in range(7)]
+                l = min([len(curve) for curve in curves])
+                curves = [curve[:l] for curve in curves]
+                y = gaussian_average(np.mean(curves, axis=0))
+                plt.plot(np.arange(len(y))+1, y, color=cm.viridis(color_nums[label]))
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+            ax.tick_params(axis='both', which='major', labelsize=tf-2)
             ax.set_ylabel('Loss',fontsize=tf)
-        if i==2:
+        if i == 1:
+            print('Plotting ground-truth loss')
+            with open('../plot_data/solver/loss_curves.pkl', "rb") as f:
+                losses = pickle.load(f)
+            for label in [0, 1, 2, 3, 4, 5, 6]:
+                y = losses[label]
+                plt.plot(np.arange(len(y))+1, gaussian_average(y), color=cm.viridis(color_nums[label]))
+            ax.set_xscale('log')
+            ax.set_yscale('linear')
+            ax.tick_params(axis='both', which='major', labelsize=tf-2)
+        if i == 2:
+            print('Plotting AB pruning complexity')
+            x = ab_data['x']
+            y = np.array(ab_data['g_mean'])#
+            gstd = np.array(ab_data['gstd'])
+            err = np.array([y*(1-1/gstd), y*(gstd-1)])
+            c = -15 # Cut off hardware-limit plateau
+            ax.errorbar(x[:c], y[:c], yerr=err[:,:c], fmt='-o')
+            ax.set_xscale('log')
+            ax.set_yscale('log')
+            ax.tick_params(axis='both', which='major', labelsize=tf-2)
             ax.set_ylabel('CPU time (s)',fontsize=tf)
         ax.set_xlabel('State rank',fontsize=tf)
         aligned_title(ax, title=titles[i],font=tf+4)
+    
+    # Colorbar:
+    norm = matplotlib.colors.LogNorm(vmin=par.min(), vmax=par.max())
+    sm = matplotlib.cm.ScalarMappable(cmap=plt.get_cmap('viridis'), norm=norm)
+    cbar = fig.colorbar(sm, ax=axs[2]) # attach to plot 2, rather than to inset
+    cbar.ax.tick_params(labelsize=tf)
+    cbar.ax.set_ylabel('Parameters', rotation=90, fontsize=tf)
 
     plt.tight_layout()
     fig.savefig('./plots/connect4_value_loss.png', dpi=900)
 
+
 def oware_value_loss():
     print('Plotting oware value loss')
-    par = np.load('src/config/parameter_counts/oware.npy')
+    
     tf =12
     # Create figure and subplots
     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
 
     with open('../plot_data/value_loss/loss_curves_oware.pkl', "rb") as f:
         loss_values, rank_values =  pickle.load(f)
-
+    par = np.load('src/config/parameter_counts/oware.npy')
     log_par = np.log(par)
     color_nums = (log_par - log_par.min()) / (log_par.max() - log_par.min())
 
